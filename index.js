3894a986dc98243c992a8a8db0ccef91d7f8a66d
JuanGro <ing.juangro@gmail.com>
2021-10-25 18:14:49 -0500

    Add test-api endpoint

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/slack/test-api', function (req, res) {
+  const { rawBody } = req;
+  const { challenge } = rawBody;
+  res.send(challenge);
+});
+
 module.exports.handler = serverless(app);
