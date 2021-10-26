4ee2c4390d93b4d93fe76c7d5d3fee05d84a3bdc
JuanGro <ing.juangro@gmail.com>
2021-10-25 21:59:18 -0500

    Add new Slack API method

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
+// const serverless = require('serverless-http');
+// const express = require('express');
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
 
-app.get('/api', function (req, res) {
-  res.send('API working!');
+require('dotenv').config();
+
+const { SLACK_SIGNING_SECRET, SLACK_TOKEN, SLACK_PORT } = process.env;
+
+const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
+const slackClient = new WebClient(SLACK_TOKEN);
+
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
+// app.get('/api', function (req, res) {
+//   res.send('API working!');
+// });
+
+// app.post('/slack/test-api', function (req, res) {
+//   try {
+//     const { challenge } = req.body;
+//     console.log(challenge);
+//     res.status(200).send(challenge);
+//   } catch (error) {
+//     res.status(500).send('Incorrect parameters');
+//   }
+// });
+
+slackEvents.on('app_mention', (event) => {
+  console.log(`Got message from user ${event.user} ${event.text}`);
+  (async () => {
+    try {
+      await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! ::tada::`})
+    } catch (error) {
+      console.log(error.data);
+    }
+  })();
 });
 
-app.post('/slack/test-api', function (req, res) {
-  try {
-    const { challenge } = req.body;
-    console.log(challenge);
-    res.status(200).send(challenge);
-  } catch (error) {
-    res.status(500).send('Incorrect parameters');
-  }
+slackEvents.on('error', console.error);
+
+slackEvents.start(SLACK_PORT || 3000).then(() => {
+  console.log(`Server started on port ${SLACK_PORT}`)
 });
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(slackEvents);
