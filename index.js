f97b63623002ca092f0ff9c705899db58600428c
JuanGro <ing.juangro@gmail.com>
2021-10-25 19:05:16 -0500

    Add express encoders

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
